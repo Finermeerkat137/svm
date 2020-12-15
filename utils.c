0db6f6c00c86d76565605de05710d9ae17a2d5f1
Sudhip Nashi <sudhip@nashi.us>
2020-12-15 15:57:47 -0600

    Do not call exit() from print_error
    This is so that there can be custom error handling if necessary later on.

--- utils.c
+++ utils.c
@@ -6,7 +6,6 @@
 void print_error(char *message) {
         fflush(stdout);
         fprintf(stderr, "\033[0;31mERROR: %s\n\033[0m", message);
-        exit(1);
 }
 
 void print_success(char *message) {
